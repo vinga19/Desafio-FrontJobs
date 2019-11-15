b8b9e09984f495b394ee15cbb65d1028e484e1c2
vinga19 <vitorhugo_rib@hotmail.com>
2019-11-15 08:50:04 -0300

    Modelo simples com funções de tratamento

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function validar() {
+	var password= document.getElementById('ps').value;
+	var email= document.getElementById('em').value;
+	
+
+	if ( password !== 'pinguin' && password!== ''){
+
+		alert('Dica de senha: É um animal bem fofinho que gosta de frio.')
+
+		
+	}
+
+	
+}
+
